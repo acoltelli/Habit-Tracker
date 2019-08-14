81d6846be5f0e3c18e0971aecb6b9b8655935389
acoltelli <acoltelli@protonmail.com>
2019-08-14 13:37:35 -0700

    combine dependencies

--- client/src/components/dashboard/SideNav/SideNav.js
+++ client/src/components/dashboard/SideNav/SideNav.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { NavLink, withRouter } from "react-router-dom";
 import { connect } from "react-redux";
-import { logoutUser } from "../../../actions/authActions";
 import { getHabits } from "../../../actions/habitsActions";
 import "./SideNav.scss";
 
